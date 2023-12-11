554e77cba35272fb5e6a7e18af066a9b1b358e35
KwilLuke <105600466+KwilLuke@users.noreply.github.com>
2023-12-10 20:45:44 -0500

    chore: update LogFn export
    Forgot to update the export from index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { ExtensionBuilder } from "./server/builder";
 import { ScalarValue } from "./proto/extension";
 import { MethodFn, InitializeFn, LogFn } from "./types/builder";
 
-export { ExtensionBuilder, ScalarValue, MethodFn, InitializeFn, LogFn as logFn };
+export { ExtensionBuilder, ScalarValue, MethodFn, InitializeFn, LogFn};
